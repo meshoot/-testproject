018e55b892035758d41a7403855c745a48db718d
mm@tdsgn.ru <mm@tdsgn.ru>
2020-07-10 22:09:50 +0700

    Add base architecture

---
+++ src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <Page/>
+</template>
+
+<script>
+    import Page from "../components/Page";
+
+	export default {
+		name: 'Main',
+		components: {
+            Page
+		}
+	}
+</script>
