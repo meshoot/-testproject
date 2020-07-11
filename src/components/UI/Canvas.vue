6abf4bdb062421c31d26339510b1b8c3902e386d
mm@tdsgn.ru <mm@tdsgn.ru>
2020-07-11 12:39:03 +0700

    Add drawing methods

--- src/components/UI/Canvas.vue
+++ src/components/UI/Canvas.vue
@@ -26,6 +26,34 @@
 			createContext() {
 				this.ctx = this.$refs.canvas.getContext('2d');
 			},
+			createRect(params, type) {
+				if (!params) {
+					return new Error('Parameters not specified for drawing')
+				}
+
+				const { x, y, width, height, fill, stroke } = params;
+
+				if (type === 'fill') {
+					this.ctx.fillRect(x, y, width, height);
+					if (fill) {
+						this.ctx.fillStyle = fill;
+					}
+				} else {
+					this.ctx.strokeRect(x, y, width, height);
+					if (stroke) {
+						this.ctx.strokeStyle = stroke;
+					}
+				}
+			},
+			clearRect(params) {
+				if (!params) {
+					return new Error('Parameters not specified for drawing')
+				}
+
+				const { x, y, width, height } = params;
+
+				this.ctx.clearRect(x, y, width, height)
+			}
 		},
 		mounted() {
 			this.createContext();
